586f2c170b60650fa82615d780db3ce0be23ddd5
MaureenChan <maureencqm@gmail.com>
2015-12-14 10:18:52 +0800

    simple blog finish

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'post_list.html', {'post_list':Post.objects.all()})
+    #return HttpResponse()
